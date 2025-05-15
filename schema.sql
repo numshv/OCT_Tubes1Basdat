71914a50e1c906948fba0fa21736debc8eedc0a8
numshv <noumisyifa@gmail.com>
2025-05-15 21:21:48 +0700

    refactor: refactor some of the table def

--- schema.sql
+++ schema.sql
@@ -1,138 +1,170 @@
 -- schema.sql
 
 -- tabel User (parent dari Buyer dan Seller)
-CREATE TABLE IF NOT EXISTS User (
-    -- id INT PRIMARY KEY NOT NULL, (kalau setuju)
-    email VARCHAR(255) PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS `User` (
+    id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 
+    email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     nama_panjang VARCHAR(255) NOT NULL,
-    tanggal_lahir DATE,
-    foto_profil VARCHAR(255),
+    tanggal_lahir DATE, -- NOT NULL,
+    foto_profil VARCHAR(255), -- NOT NULL
     tipe ENUM('Buyer', 'Seller') DEFAULT 'Buyer'
 );
 
 CREATE TABLE IF NOT EXISTS Pertemanan(
-    email VARCHAR(255) NOT NULL,
-    email_friend VARCHAR(255) NOT NULL,
-    PRIMARY KEY (email, email_friend)
+    id_user INT NOT NULL,
+    id_user_teman INT NOT NULL,
+    PRIMARY KEY (id_user, id_user_teman)
 
-    -- FOREIGN KEY (email_friend) REFERENCES User(email),
-    -- FOREIGN KEY (email) REFERENCES User(email)
-    -- perlu pake on delete cascade on update cascade gk ya? apa gk boleh?
-    -- both email sama email_friend better keduanya jadi PK atau tabel gk ada PK samsek?
+    FOREIGN KEY (id_user) REFERENCES `User`(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (id_user_teman) REFERENCES `User`(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Seller(
-    email VARCHAR(255) NOT NULL PRIMARY KEY,
+    id_user INT NOT NULL PRIMARY KEY,
     ktp VARCHAR(255) NOT NULL,
     foto_diri VARCHAR(255) NOT NULL,
-    is_verified BOOLEAN NOT NULL,
+    is_verified BOOLEAN NOT NULL DEFAULT FALSE,
 
-    FOREIGN KEY (email) REFERENCES User(email)
+    FOREIGN KEY (id_user) REFERENCES `User`(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS InstTelp(
-    email VARCHAR(255) NOT NULL,
+    id_user INT NOT NULL PRIMARY KEY,
     nomor_telpon VARCHAR(20) NOT NULL,
-    PRIMARY KEY (email, nomor_telpon),
+    PRIMARY KEY (id_user, nomor_telpon),
 
-    FOREIGN KEY (email) REFERENCES User(email)
-    -- perlu pake on delete cascade on update cascade gk ya? apa gk boleh?
-    -- email better keduanya jadi PK atau tabel gk ada PK samsek?
+    FOREIGN KEY (id_user) REFERENCES `User`(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Buyer(
-    email VARCHAR(255) PRIMARY KEY NOT NULL,
-    FOREIGN KEY (email) REFERENCES User(email)
+    id_user INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (id_user) REFERENCES `User`(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Produk(
     id_produk INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nama VARCHAR(255) NOT NULL,
-    deskripsi VARCHAR(255),
-    email VARCHAR(255) NOT NULL,
+    deskripsi TEXT,
+    id_seller INT NOT NULL,
 
-    FOREIGN KEY (email) REFERENCES Seller(email)
+    FOREIGN KEY (id_seller) REFERENCES Seller(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS VarianProduk(
     sku VARCHAR(255) NOT NULL,
     id_produk INT NOT NULL,
     nama_varian VARCHAR(255) NOT NULL,
-    harga INT NOT NULL,
-    stok INT NOT NULL,
+    harga INT NOT NULL CHECK (harga >= 0),
+    stok INT NOT NULL DEFAULT 0 CHECK (harga >= 0),
 
     PRIMARY KEY (sku, id_produk),
 
     FOREIGN KEY (id_produk) REFERENCES Produk(id_produk)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Keranjang(
-    -- ini sementara, blm fix masalah PK
-    email VARCHAR(255) NOT NULL,
+    id_user INT NOT NULL,
     sku VARCHAR(255) NOT NULL,
     id_produk INT NOT NULL,
 
-    PRIMARY KEY(email, sku, id_produk),
+    PRIMARY KEY(id_user, sku, id_produk),
 
-    kuantitas INT NOT NULL,
+    kuantitas INT NOT NULL DEFAULT 1,
 
-    FOREIGN KEY (email) REFERENCES Buyer(email),
-    FOREIGN KEY (sku) REFERENCES VarianProduk(sku),
+    FOREIGN KEY (id_user) REFERENCES Buyer(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (sku) REFERENCES VarianProduk(sku)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     FOREIGN KEY (id_produk) REFERENCES Produk(id_produk)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Wishlist(
     -- ini sementara, blm fix masalah PK
-    email VARCHAR(255) NOT NULL,
+    id_user INT NOT NULL,
     id_produk INT NOT NULL,
 
-    -- PRIMARY KEY(email, id_produk),
+    PRIMARY KEY(id_user, id_produk),
 
-    FOREIGN KEY (email) REFERENCES Buyer(email),
+    FOREIGN KEY (id_user) REFERENCES Buyer(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     FOREIGN KEY (id_produk) REFERENCES Produk(id_produk)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Alamat(
-    email VARCHAR(255) NOT NULL,
+    id_user INT NOT NULL,
     id_alamat INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     provinsi VARCHAR(255) NOT NULL,
     kota VARCHAR(255) NOT NULL,
     jalan VARCHAR(255) NOT NULL,
-    is_utama BOOLEAN NOT NULL,
+    is_utama BOOLEAN NOT NULL DEFAULT FALSE,
 
-    FOREIGN KEY (email) REFERENCES Buyer(email) 
+    FOREIGN KEY (id_user) REFERENCES Buyer(id_user)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
 );
 
 CREATE TABLE IF NOT EXISTS Orders(
     id_order INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     id_alamat INT NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    status_order ENUM('pesanan belum dibayar', 'pesanan sedang disiapkan', 'pesanan sedang dikirim', 'pesanan sampai', 'pesanan dibatalkan') NOT NULL,
+    id_user INT NOT NULL,
+    status_order ENUM('pesanan belum dibayar', 'pesanan sedang disiapkan', 'pesanan sedang dikirim', 'pesanan sampai', 'pesanan dibatalkan') 
+        NOT NULL DEFAULT 'pesanan belum dibayar',
     metode_pembayaran VARCHAR(255) NOT NULL,
     metode_pengiriman VARCHAR(255) NOT NULL,
-    catatan VARCHAR(255) NOT NULL,
-    waktu_pemesanan TIMESTAMP NOT NULL,
-    kuantitas INT NOT NULL DEFAULT 1,
-    sku VARCHAR(255),
+    catatan VARCHAR(255),
+    waktu_pemesanan TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (id_alamat) REFERENCES Alamat(id_alamat),
-    FOREIGN KEY (email) REFERENCES Buyer(email)
+    FOREIGN KEY (id_alamat) REFERENCES Alamat(id_alamat)
+        -- ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (id_user) REFERENCES Buyer(id_user)
+        -- ON DELETE CASCADE
+        ON UPDATE CASCADE 
+    -- bingung biasanya kalau alamat/user dihapus histori belinya ilang gk?
 
-    -- FK SKU?
 );
 
 CREATE TABLE IF NOT EXISTS InstProduk(
     id_order INT NOT NULL,
     id_produk INT NOT NULL,
+    sku VARCHAR(255) NOT NULL,
 
-    -- PRIMARY KEY(id_order, id_produk),
+    PRIMARY KEY(id_order, id_produk, sku),
 
-    FOREIGN KEY (id_order) REFERENCES Orders(id_order),
+    kuantitas INT NOT NULL DEFAULT 1,
+
+    FOREIGN KEY (id_order) REFERENCES Orders(id_order)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     FOREIGN KEY (id_produk) REFERENCES Produk(id_produk)
-
-    -- FK SKU?
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (sku) REFERENCES VarianProduk(sku)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 
@@ -140,16 +172,20 @@ CREATE TABLE IF NOT EXISTS InstTag(
     id_produk INT NOT NULL,
     tag VARCHAR(255) NOT NULL,
 
-    -- PRIMARY KEY (tag, id_produk),
+    PRIMARY KEY (tag, id_produk),
 
     FOREIGN KEY (id_produk) REFERENCES Produk(id_produk)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS InstGambar(
     id_produk INT NOT NULL,
     gambar VARCHAR(255) NOT NULL,
 
-    -- PRIMARY KEY (gambar, id_produk),
+    PRIMARY KEY (gambar, id_produk),
 
     FOREIGN KEY (id_produk) REFERENCES Produk(id_produk)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
